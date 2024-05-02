ad7d94a7b1c8a8d0fa1b11e322b7a9714e6fd291
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-05-02 23:51:30 +0530

    Terraform-expense = RDS creation

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "key_id" {
+  default = "arn:aws:kms:us-east-1:251895722409:key/3cc2468d-37e7-440b-b77b-eb9d2ac1325c"
+}
+
+variable "parameters" {
+  default = {
+    "dev.expense.rds.username" = "admin1"
+  }
+}
+
+
+##Note : In organization we dont keep passwords in Git repo at any cost, Here in Lab, Just for learnability purpose we are declaring them here and hardcoding in git repos
+variable "secrets" {
+  default = {
+    "dev.expense.rds.password" = "ExpenseApp123"
+  }
+}
