81467fb89c45f16e8e7b84e380d9cca3e403cb2d
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-05-07 13:42:45 +0530

    Terraform-expense = DNS Record creation

--- variables.tf
+++ variables.tf
@@ -6,6 +6,7 @@ variable "parameters" {
   default = {
     "dev.expense.rds.username" = "admin1"
     "dev.expense.rds.endpoint" = "dev-expense-rds.c3iaa0466epk.us-east-1.rds.amazonaws.com"
+    "dev.expense.frontend.backend_endpoint" = "https://backend-dev.tejas1996.online/"
   }
 }
 
